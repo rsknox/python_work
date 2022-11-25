da4817eb21105debf40d769fd2696d545bc89488
R.S <rsknox@gmail.com>
2022-11-25 12:58:38 -0600

    added the for_while_if file

---
+++ for_while_if.py
@@ -0,0 +1,37 @@
+# for loops, while loops, and if statement practice
+# grade = ['C/AB', 'C/Amn', 'C/A1C','C/SrA','C/SSgt']
+# cadet_name = ['Argabright', 'Conn, S.', 'Conn, V.', 'Dahlen', 'Clark']
+# for i in grade:
+#     print(i)
+# for name in cadet_name:
+#     print(name)
+#
+# leng = len(cadet_name)
+# print (leng)
+#
+# for j in range(0, leng):
+#     print(cadet_name[j])
+#
+# for m in range(leng, 0, -1):
+#     print("Inv?: ", cadet_name[m-1])
+#
+# c = 0
+# while (c < leng):
+#     print("cadet: ", cadet_name[c])
+#     c = c + 1
+#
+# k = leng - 1
+# while (k > -1):
+#     print("inv list: ", cadet_name[k])
+#     k = k - 1
+#
+# dictionary
+ach_grd = {0:'C/AB', 1:'C/Amn', 2:'C/A1C', 3:'C/SrA', 4:'C/SSgt', 5:'C/TSgt',
+           6:'C/MSgt', 7:'C/SMSgt', 8:'C/CMSgt', 9:'C/CMSgt', 10:'C/2nd Lt'}
+print(ach_grd[5])
+len_ag = len(ach_grd)
+print (len_ag), cadet_name[k])
+ach_grd = {0:'C/AB', 1:'
+for n in range(0, len_ag):
+    print(ach_grd[n])
+
